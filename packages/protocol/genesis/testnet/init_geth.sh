fbeb4126d93e6581cf41f57b4929fe0d46c4723e
Must479 <67345599+must479@users.noreply.github.com>
2024-08-22 03:07:57 +0700

    Update init_geth.sh

--- packages/protocol/genesis/testnet/init_geth.sh
+++ packages/protocol/genesis/testnet/init_geth.sh
@@ -15,6 +15,6 @@ geth --datadir /root/.ethereum \
   --http.api debug,eth,net,web3,txpool,miner \
   --allow-insecure-unlock \
   --password /host/password \
-  --miner.etherbase 0xdf08f82de32b8d460adbe8d72043e3a7e25a3b39 \
-  --unlock 0xdf08f82de32b8d460adbe8d72043e3a7e25a3b39 \
+  --miner.etherbase 0x2D815240A61731c75Fa01b2793E1D3eD09F289d0 \
+  --unlock 0x2D815240A61731c75Fa01b2793E1D3eD09F289d0 \
   --mine

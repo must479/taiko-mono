e3a8d9f85809ab979d2d8a888e9d1631b6c0731d
Must479 <67345599+must479@users.noreply.github.com>
2026-03-03 18:12:12 +0700

    Update miner etherbase and unlock address

--- packages/protocol/genesis/testnet/init_geth.sh
+++ packages/protocol/genesis/testnet/init_geth.sh
@@ -15,6 +15,6 @@ geth --datadir /root/.ethereum \
   --http.api debug,eth,net,web3,txpool,miner \
   --allow-insecure-unlock \
   --password /host/password \
-  --miner.etherbase 0x2D815240A61731c75Fa01b2793E1D3eD09F289d0 \
-  --unlock 0x2D815240A61731c75Fa01b2793E1D3eD09F289d0 \
+  --miner.etherbase 0xD575856610bD93A9f1AD89bE4a17E90A7bB331C5 \
+  --unlock 0xD575856610bD93A9f1AD89bE4a17E90A7bB331C5 \
   --mine
